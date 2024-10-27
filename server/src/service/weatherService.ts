7ae8f3ebb2825936e1e05fba935cb233c366e480
Caryn Carter <carter.caryn@gmail.com>
2024-10-26 19:22:21 -0500

    added buildGeoCode query function

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -41,19 +41,22 @@ class Weather {
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
-  private baseURL?: string;
-
-  private apiKey?: string;
-
-  //private city?: string;
+  private baseURL: string;
+  private apiKey: string;
 
   constructor() {
     this.baseURL = process.env.API_BASE_URL || 'https://api.openweathermap.org';
-
-    this.apiKey = process.env.API_KEY || '59c998cb2e2f6af29e6145721795b2df';
-
-    //this.city = process.env.CITY_NAME || ""; 
+    this.apiKey = process.env.API_KEY || 'YOUR_API_KEY';
+  }
+  
+  // TODO: Complete getWeatherForCity method
+  // async getWeatherForCity(city: string) {}
+  
+  
+ // TODO: Create buildGeocodeQuery method
+  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {
+    return `${this.baseURL}/geo/1.0/direct?limit=1&appid=${this.apiKey}`;
   }
 
   // TODO: Create fetchLocationData method
@@ -77,8 +80,7 @@ class WeatherService {
 
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+ 
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
@@ -89,8 +91,7 @@ class WeatherService {
   // private parseCurrentWeather(response: any) {}
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+
 }
 
 export default new WeatherService();
